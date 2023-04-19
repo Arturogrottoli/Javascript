28f6e1e76dba55150e7dc7ed2bc32921f8150ab7
ArturoEmonk <arturogrottoli@gmail.com>
2023-04-19 19:31:49 -0300

    Varibles

--- app.js
+++ app.js
@@ -4,7 +4,46 @@
 console.log("HOLA MUNDO DESDE UN ARCHIVO EXTERNO!")
 console.log("HOLA MUNDO DESDE UN ARCHIVO EXTERNO! 2")
 /*
-ACA COMENTARIOS
-QUE SEAN ESCALONADOS
+DECLARACION DE VARIABLES
 
-*/gi
+*/
+
+var nombre = prompt("Ingrese su nombre", "")
+document.write("El nombre ingresado es ", nombre)
+
+let numero = 10+3;
+console.log(--numero)
+let booleano = true;
+const pi = 3.1416;
+var x;
+
+var prueba = Number('A');
+console.log(prueba);
+
+
+
+resultado = nombre+numero;
+var booleano2 = isFinite(numero); //Devuelve true o false
+
+
+
+console.log(numero);
+console.log(nombre);
+console.log(typeof numero);
+console.log(booleano2);  
+console.log(typeof nombre);
+console.log(typeof x);
+console.log(typeof resultado);
+console.log(booleano);
+console.log(pi);
+console.log(x);
+
+nombre = 20;
+pi = 25;
+
+console.log(nombre);
+console.log(pi);
+
+
+console.log("La nueva variable resultado es igual a:", resultado)
+
